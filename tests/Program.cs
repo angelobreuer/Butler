b410caaa57edb22768670986cd0ef5579a8171db
Angelo Breuer <angelobr@outlook.de>
2019-08-12 21:03:05 +0200

    Added basic project structure

---
+++ tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Butler.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
